021bf2051e228f85d2085df6963e943d78c0eaea
Erkinovadham <adhamjonerkinov65@gmail.com>
2023-05-11 22:34:04 +0500

    nimadir

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,15 +2,40 @@ import "./App.css";
 import Header from "../header/header"
 import Field from "../field/field"
 import Button from "../button/button"
+import Component from "react";
 
-function App() {
+class User extends Component {
+  render() {
+    const { firstName, lastName, link } = this.props
+
+    return (
+      <div>
+        <h1>
+          mening ismim - {firstName}, sharifim - {lastName}
+        </h1>
+        <a href={link}>Youtube kanalim</a>
+      </div>
+    );
+  }
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Field />
-      <Button />
+    <div>
+      <User firstName="Adham" lastName="Erkinov" link="youtube.com" />
+      <User firstName="Omar" lastName="abdulloh" link="google.com" />
     </div>
   );
-}
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Field />
+//       <Button />
+//     </div>
+//   );
+// }
 
 export default App;
