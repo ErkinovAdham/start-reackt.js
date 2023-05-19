90128ed88814264ef0953fcf91c98bacc06956b8
Erkinovadham <adhamjonerkinov65@gmail.com>
2023-05-19 23:28:35 +0500

    i am programmer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,18 +2,39 @@ import "./App.css";
 import Header from "../header/header"
 import Field from "../field/field"
 import Button from "../button/button"
-import Component from "react";
+import {Component} from "react";
 
 class User extends Component {
+    constructor(props) {
+      supper(props)
+      this.state = {
+        counter:10,
+      }
+    }
+
+    clickMandler = () => {
+      this.setState.counter +1
+    }
+}
+
+
   render() {
     const { firstName, lastName, link } = this.props
 
     return (
-      <div>
-        <h1>
+      <div className="m-50 mx-5">
+      <div className="border p-3 mt-5">
+        <h4>
           mening ismim - {firstName}, sharifim - {lastName}
-        </h1>
+        </h4>
         <a href={link}>Youtube kanalim</a>
+        <div className="mt-3">
+            <button onClick={this.clickMandler} className="btn btn-success">
+                Click
+            </button>
+            <p>{this.this.state.counter}</p>
+        </div>
+      </div>
       </div>
     );
   }
